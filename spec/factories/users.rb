69de93cb14d92356de24bc0df653fa591910abbb
zab3 <xelvis03@gmail.com>
2020-10-07 23:01:51 +0900

    FactoryBotの生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    email                  {Faker::Interner.free_mail}
+    password               {Faker::Internet.free_email}
+    password_confirmation  {password}
+    nickname               {Faker::Name.name}
+    name_first             {Faker::Name.first_name}
+    name_last              {Faker::Name.last_name}
+    name_first_kana        {Faker::Name.first_kana_name}    
+    name_last_kana         {Faker::Name.last_kana_nane}              
   end
 end
