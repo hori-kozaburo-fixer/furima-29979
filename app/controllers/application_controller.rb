192751d68679a31436a1f97788e70ad5c8e989b1
zab3 <xelvis03@gmail.com>
2020-10-08 16:34:37 +0900

    rubocopによる修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_first, :name_last, :name_first_kana, :name_last_kana, :birth_day])
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email                  {Faker::Internet.free_email}
-    password               {Faker::Internet.password(min_length: 6)}
-    password_confirmation  {password}
-    nickname               {Faker::Name.name}
-    name_first             {ForgeryJa(:name).first_name}
-    name_last              {ForgeryJa(:name).last_name}
-    name_first_kana        {ForgeryJa(:name).first_name(:to => ForgeryJa::KANA)}    
-    name_last_kana         {ForgeryJa(:name).last_name(:to => ForgeryJa::KANA)} 
-    birth_day              {"2020-01-01"}             
+    email                  { Faker::Internet.free_email }
+    password               { Faker::Internet.password(min_length: 6) }
+    password_confirmation  { password }
+    nickname               { Faker::Name.name }
+    name_first             { ForgeryJa(:name).first_name }
+    name_last              { ForgeryJa(:name).last_name }
+    name_first_kana        { ForgeryJa(:name).first_name(to: ForgeryJa::KANA) }
+    name_last_kana         { ForgeryJa(:name).last_name(to: ForgeryJa::KANA) }
+    birth_day              { '2020-01-01' }
   end
 end
