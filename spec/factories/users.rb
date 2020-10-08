5cbdf1e428d37273f802f0002cfc5be576d13889
zab3 <xelvis03@gmail.com>
2020-10-08 11:57:02 +0900

    FactoryBot再修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,10 @@ FactoryBot.define do
     password               {Faker::Internet.password(min_length: 6)}
     password_confirmation  {password}
     nickname               {Faker::Name.name}
-    name_first             {Faker::Name.first_name}
-    name_last              {Faker::Name.last_name}
+    name_first             {ForgeryJa(:name).first_name}
+    name_last              {ForgeryJa(:name).last_name}
     name_first_kana        {ForgeryJa(:name).first_name(:to => ForgeryJa::KANA)}    
-    name_last_kana         {ForgeryJa(:name).last_name(:to => ForgeryJa::KANA)}              
+    name_last_kana         {ForgeryJa(:name).last_name(:to => ForgeryJa::KANA)} 
+    birth_day              {"2020-01-01"}             
   end
 end
