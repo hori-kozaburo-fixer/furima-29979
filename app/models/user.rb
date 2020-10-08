9a88771b2675ac8c082510715ed7752cbae66202
zab3 <xelvis03@gmail.com>
2020-10-08 11:57:18 +0900

    passwordのバリデーション訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i,message:"Include both letters and numbers."}      
+  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,message:"Include both letters and numbers."}      
 
   with_options presence: true do
     validates :nickname
