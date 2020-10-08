0211f5cd0f5d36c99666c5c27e3be1be35ce38d6
zab3 <xelvis03@gmail.com>
2020-10-08 19:08:39 +0900

    item,userモデルのアソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :sender_area
+  belongs_to_active_hash :number_of_day
+  belongs_to :user
+
+  with_options presence: true, do
+    validates :name,
+    validates :text
+    validates :category_id,      numericality: { other_than: 1 }  
+    validates :statusid,         numericality: { other_than: 1 }
+    validates :delivery_fee_id,  numericality: { other_than: 1 }
+    validates :sender_area_id,   numericality: { other_than: 1 }
+    validates :number_of_day_id, numericality: { other_than: 1 }
+    validates :money,
+    validates :user_id,          foreign_key: true
+  end
 end
