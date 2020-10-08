203015cee89501160f15d354789e50464ce6108e
zab3 <xelvis03@gmail.com>
2020-10-08 22:40:49 +0900

    modelのバグ調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
@@ -8,15 +8,14 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  with_options presence: true, do
-    validates :name,
+  with_options presence: true do
+    validates :name
     validates :text
     validates :category_id,      numericality: { other_than: 1 }  
-    validates :statusid,         numericality: { other_than: 1 }
+    validates :status_id,         numericality: { other_than: 1 }
     validates :delivery_fee_id,  numericality: { other_than: 1 }
     validates :sender_area_id,   numericality: { other_than: 1 }
     validates :number_of_day_id, numericality: { other_than: 1 }
-    validates :money,
-    validates :user_id,          foreign_key: true
+    validates :money
   end
 end
