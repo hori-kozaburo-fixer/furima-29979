89afefc222a494c65481b0b63c186453b2887b64
zab3 <xelvis03@gmail.com>
2020-10-08 23:33:38 +0900

    バリデーションの再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,14 @@ class Item < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :text
-    validates :category_id,      numericality: { other_than: 1 }  
-    validates :status_id,         numericality: { other_than: 1 }
-    validates :delivery_fee_id,  numericality: { other_than: 1 }
-    validates :sender_area_id,   numericality: { other_than: 1 }
-    validates :number_of_day_id, numericality: { other_than: 1 }
-    validates :money
+    validates :money,            format: { with: /\A[0-9]+\z/,message: "Half-width number"},numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_000_000, message: "Out of setting range" }
+    validates :category_id,      numericality: { other_than: 1,message: "Select" }
+    validates :status_id,        numericality: { other_than: 1,message: "Select" }
+    validates :delivery_fee_id,  numericality: { other_than: 1,message: "Select" }
+    validates :sender_area_id,   numericality: { other_than: 1,message: "Select" }
+    validates :number_of_day_id, numericality: { other_than: 1,message: "Select" }
   end
 end
