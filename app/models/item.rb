1680417a076f5cf872885120ca53030c04c0f171
zab3 <xelvis03@gmail.com>
2020-10-09 14:41:43 +0900

    半角判定をnumericality一つに集約

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :text
-    validates :money,            format: { with: /\A[0-9]+\z/,message: "Half-width number"},numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Out of setting range" }
+    validates :money,            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Out of setting range" }
     validates :category_id,      numericality: { other_than: 1,message: "Select" }
     validates :status_id,        numericality: { other_than: 1,message: "Select" }
     validates :delivery_fee_id,  numericality: { other_than: 1,message: "Select" }
