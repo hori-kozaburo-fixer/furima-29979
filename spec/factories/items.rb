1ba62e8c73ee7bff9de487b65cd15e17bc634402
zab3 <xelvis03@gmail.com>
2020-10-09 11:54:46 +0900

    itemのfactorybot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Name.name }
+    text                  { Faker::Lorem.sentence }
+    category_id           { "1" }
+    status_id             { "1" }
+    delivery_fee_id       { "1" }
+    sender_area_id        { "1" }
+    number_of_day_id      { "1" }
+    money                 { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
   end
 end
