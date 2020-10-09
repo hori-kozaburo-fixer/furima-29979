88b5b3741ce006de3c7dd65af184f588c5dd3933
zab3 <xelvis03@gmail.com>
2020-10-09 15:17:30 +0900

    activehashを用いたテストをID：１が選択された時に変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,31 +30,31 @@ RSpec.describe Item, type: :model do
     end
 
     it 'カテゴリーの情報が必須であること' do
-      @item.category_id = ""
+      @item.category_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Category Select")
     end
 
     it '商品の状態についての情報が必須であること' do
-      @item.status_id = ""
+      @item.status_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Status Select")
     end
 
     it '配送料の負担についての情報が必須であること' do
-      @item.delivery_fee_id = ""
+      @item.delivery_fee_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery fee Select")
     end
 
     it '発送元の地域についての情報が必須であること' do
-      @item.sender_area_id = ""
+      @item.sender_area_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Sender area Select")
     end
 
     it '発送までの日数についての情報が必須であること' do
-      @item.number_of_day_id = ""
+      @item.number_of_day_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Number of day Select")
     end
