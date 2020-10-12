066ad3a0934fe6850171eac7c72caf274db8ade2
zab3 <xelvis03@gmail.com>
2020-10-12 14:07:52 +0900

    ログインしていないユーザー及び自身が出品した商品の購入ページには遷移できないように設定。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,19 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index 
+    item_user = Item.find(params[:item_id]) 
+    if current_user.id == item_user.user_id
+      redirect_to root_path
+    else
     @purchase_address = PurchaseAddress.new 
+    end
   end
 
   def create 
     @purchase_address = PurchaseAddress.new(purchase_params)
-    binding.pry
     if @purchase_address.valid?
       pay_item
-      # binding.pry
       @purchase_address.save
       return redirect_to root_path
     else
