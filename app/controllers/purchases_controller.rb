e3dc0bb27880e0a991179772b30bb19c823949d7
zab3 <xelvis03@gmail.com>
2020-10-12 13:50:09 +0900

    フォームオブジェクト及びクレジット決済ができるようコントローラーを修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,15 @@
 class PurchasesController < ApplicationController
   def index 
-    @purchase = Purchase.new 
+    @purchase_address = PurchaseAddress.new 
   end
 
   def create 
-    
-    @purchase = Purchase.new(purchase_params)
-    if @purchase.valid?
-      @purchase.save
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    binding.pry
+    if @purchase_address.valid?
+      pay_item
+      # binding.pry
+      @purchase_address.save
       return redirect_to root_path
     else
       render 'index'
@@ -17,7 +19,17 @@ class PurchasesController < ApplicationController
   private
   
   def purchase_params
-    params.require(:purchase).merge(user_id: current_user.id, item_id: item.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :tel).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id] )
+  end
+
+  def pay_item
+    @item = Item.find(purchase_params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+        amount: @item.money,
+        card: purchase_params[:token],
+        currency: 'jpy'
+    )
   end
 
 end
