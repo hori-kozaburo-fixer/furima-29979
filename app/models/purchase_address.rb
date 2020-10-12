a356afe7b6fe706b9cafc376a328790605e100a9
zab3 <xelvis03@gmail.com>
2020-10-12 16:52:46 +0900

    バリデーションの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,13 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
 
-  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :tel, :purchase_id, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :tel, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/,message: "Input correctly"}
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+    validates :municipality
+    validates :house_number
+    validates :tel, format: { with: /\A\d{10,11}\z/, message: 'input only number'}
+  end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    binding.pry
-    address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number,building_name: building_name, tel: tel, purchase_id: purchase.id)
-    binding.pry
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number,building_name: building_name, tel: tel, purchase_id: purchase.id)
   end
 
 end
