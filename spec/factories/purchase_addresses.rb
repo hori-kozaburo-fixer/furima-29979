789b947c750c3bc07626c65d32277a787685d634
zab3 <xelvis03@gmail.com>
2020-10-12 18:00:14 +0900

    テストコード作成

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    postal_code   { '123-4567' }
+    prefecture_id { 2 } 
+    municipality  { '名古屋市' }
+    house_number  { '青山3−32−3' }
+    building_name { 'レジデンス100' }
+    tel           { '09012345678' }
+    token         { 'tok_abcdefghijk00000000000000000' }
   end
 end
