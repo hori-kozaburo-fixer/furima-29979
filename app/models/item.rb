dfb8c154114179d367f777a1c1afd7f6f2fdd5ed
zab3 <xelvis03@gmail.com>
2020-10-13 12:48:50 +0900

    purchaseテーブルにアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :number_of_day
   has_one_attached :image
   belongs_to :user
+  has_one :purchase
 
   with_options presence: true do
     validates :image
