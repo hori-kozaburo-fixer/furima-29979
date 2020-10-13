5608d9f5e8c436b4ed1139809575a88b58d6c57f
zab3 <xelvis03@gmail.com>
2020-10-13 21:00:58 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:show, :edit, :update]
+  
   def index
     @items = Item.order('created_at DESC')
   end
