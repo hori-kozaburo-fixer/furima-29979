77b136e11ab96fc0a7b1773d86ccf28beb2bbf4b
zab3 <xelvis03@gmail.com>
2020-10-14 10:38:57 +0900

    条件分岐再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,8 +30,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    if @item.valid?
+    if current_user.id == @item.user.id
+      @item.destroy
       redirect_to root_path
     else
       render :show
