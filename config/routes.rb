5987e29ea24f2f452e61824dc532ea6c9b2550f6
zab3 <xelvis03@gmail.com>
2020-10-18 13:01:29 +0900

    複数形に変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :comment, only: [:create]
+    resources :comments, only: [:create]
     resources :purchases, only: [:index, :new, :create]
   end
 
